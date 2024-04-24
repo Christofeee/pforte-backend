10e0b846fbe6978da463d3d97cde7e7bc6af00d6
Christopher Nicholas ( Zin Phyo Min) <chrizt0202@gmail.com>
2024-04-24 10:31:57 +0700

    initial commit - Models, Controllers, APIs initiated

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\AssessmentController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\SubmissionController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::resource('users', UserController::class);
+Route::resource('classrooms', ClassroomController::class);
+Route::resource('announcements', AnnouncementController::class);
+Route::resource('modules', ModuleController::class);
+Route::resource('assessments', AssessmentController::class);
+Route::resource('grades', GradeController::class);
+Route::resource('submissions', SubmissionController::class);
